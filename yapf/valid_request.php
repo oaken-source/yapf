6af78175eb31bb1382a928c80cc057beedd8e67c
Andreas Grapentin <andreas@grapentin.org>
2014-04-24 13:12:29 +0200

    updated copyright year

--- yapf/valid_request.php
+++ yapf/valid_request.php
@@ -3,7 +3,7 @@
 /******************************************************************************
  *                                    yapf                                    *
  *                                                                            *
- *    Copyright (C) 2013  Karl Kronberger, Andreas Grapentin                  *
+ *    Copyright (C) 2013 - 2014  Karl Kronberger, Andreas Grapentin           *
  *                                                                            *
  *    This program is free software: you can redistribute it and/or modify    *
  *    it under the terms of the GNU General Public License as published by    *
